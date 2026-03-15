a02e207a5a09b92de3a278ee48ad01ba0ac09198
19Juanjo <juanmuno1904@gmail.com>
2026-03-14 21:51:17 -0500

    chore: initial project setup

---
+++ src/main/java/com/example/inventarioGangazo/InventarioGangazoApplication.java
@@ -0,0 +1,13 @@
+package com.example.inventarioGangazo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioGangazoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioGangazoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/inventarioGangazo/InventarioGangazoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inventarioGangazo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioGangazoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
