696f792d45a1f9928bad5bc5d993236bc065b67e
19Juanjo <juanmuno1904@gmail.com>
2026-03-16 22:56:43 -0500

    feat: add entitys order,rol,ShoppingCart,ShoppingCartItem

---
+++ src/main/java/entity/Rol.java
@@ -0,0 +1,22 @@
+package entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "rol")
+public class Rol {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column( name = "id")
+    private Long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+}
